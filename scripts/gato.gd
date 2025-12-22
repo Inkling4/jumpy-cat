2d0956ea42e5d36bf815ccd37e14189b29b9bfe5
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-12-22 21:18:38 +0100

    started drag

--- scripts/gato.gd
+++ scripts/gato.gd
@@ -1,4 +1,8 @@
+class_name Gato
 extends CharacterBody2D
+
+# Is true while player holds the finger down/is dragging the aim.
+var is_dragging : bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +12,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
+	
+	
+func _input(event: InputEvent) -> void:
+	
+	if (event.is_action_just_pressed("player_drag")):
+		is_dragging = true
