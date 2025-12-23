ed127bc7ae2e662a593a2a96333b0f7687560db5
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-12-23 20:54:53 +0100

    platform progress fix

--- scripts/moving_platform.gd
+++ scripts/moving_platform.gd
@@ -6,17 +6,36 @@ extends Node2D
 
 @export var forward : bool = true
 
+var length : float
+
+# Sets path to max value, stores the distance at the time in a property.
+func _ready() -> void:
+	if (path_follow_2d):
+		var _start_progress = path_follow_2d.progress
+		path_follow_2d.progress_ratio = 1
+		length = path_follow_2d.progress
+		path_follow_2d.progress = _start_progress
+	else:
+		print("Moving Platform _ready failed! (null reference)")
 
 func _physics_process(delta: float) -> void:
 	if (path_follow_2d):
+		var _current_progress : float = path_follow_2d.progress
 		# Moves
 		if (forward):
-			path_follow_2d.progress += platform_speed * delta
+			_current_progress += platform_speed * delta
+			if (_current_progress > length):
+				path_follow_2d.progress = length
+				forward = false
+			else:
+				path_follow_2d.progress = _current_progress
 		else:
-			path_follow_2d.progress -= platform_speed * delta
-		# Reverses direction at the edges
-		if (path_follow_2d.progress >= 1):
-			forward = false
-		if (path_follow_2d.progress <= 0):
-			forward = true
+			_current_progress -= platform_speed * delta
+			if (_current_progress <= 0):
+				path_follow_2d.progress = 0
+				forward = true
+			else:
+				path_follow_2d.progress = _current_progress
+		
+		print ("Platform progress: ", path_follow_2d.progress)
 	
