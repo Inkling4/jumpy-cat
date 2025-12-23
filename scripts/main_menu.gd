e5277847e8743d093cdf908b7fc8b7afa9a6129b
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-12-23 20:00:42 +0100

    comment change

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
+# Level that opens when you press the play button.
 @export var default_level : PackedScene
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 func _on_play_button_released() -> void:
